45856bdee8c4f7b0e41e87d097b8a0787246cdca
kiang <kiange@gmail.com>
2024-10-02 01:43:42 +0800

    add mirror media data to incomes reports

--- scripts/03_report_all_income.php
+++ scripts/03_report_all_income.php
@@ -8,6 +8,80 @@ $pathBiz = dirname(__DIR__) . '/report/incomes/business';
 if (!file_exists($pathBiz)) {
     mkdir($pathBiz, 0777, true);
 }
+foreach (glob(dirname(__DIR__) . '/data/mirror-media/*.csv') as $csvFile) {
+    $fh = fopen($csvFile, 'r');
+    $head = fgetcsv($fh, 2048);
+    while ($line = fgetcsv($fh, 2048)) {
+        $data = array_combine($head, $line);
+        if (isset($data['屆數'])) {
+            /**
+             * [屆數] => 7
+    [姓名] => 丁守中
+    [捐贈者/支出對象] => 富廣開發股份有限公司
+    [身份證/統一編] => 80015808
+    [收入金額] => 300000
+    [交易日期] => 096/12/26
+    [地址] => 新竹縣竹北市
+    [填答數] => 7
+    [其他] => 身份證/統一編：80015808=7 =1
+             */
+            if ($data['收入金額'] < 10000) {
+                continue;
+            }
+            if (strlen($data['身份證/統一編']) !== 8) {
+                continue;
+            }
+            if($data['屆數'] == 7) {
+                $data['屆數'] = '097年立法委員選舉';
+            } else {
+                $data['屆數'] = '101年立法委員選舉';
+            }
+            $targetFile = $pathBiz . '/' . $data['身份證/統一編'] . '.csv';
+            if (!file_exists($targetFile)) {
+                $oFh = fopen($targetFile, 'w');
+                fputcsv($oFh, ['選舉', '捐贈對象', '捐贈人', '捐贈日期', '捐贈金額']);
+                fclose($oFh);
+            }
+            $oFh = fopen($targetFile, 'a');
+            fputcsv($oFh, [$data['屆數'], $data['姓名'], $data['捐贈者/支出對象'], $data['交易日期'], $data['收入金額']]);
+            fclose($oFh);
+        } else {
+            /**
+             *     [候選人] => 黃珊珊
+    [推薦政黨] => 親民黨
+    [當選註記] => 
+    [序號] => 1
+    [交易日期] => 105/01/15
+    [收支科目] => 營利事業捐贈收入
+    [統一編號] => 38018607
+    [捐贈者／支出對象] => 正大尼龍工業股份有限公司
+    [收入金額] => 200000
+    [支出金額] => 
+    [金錢類] => 是
+    [地址] => 臺北市中山區
+    [P] =>
+             */
+            if ($data['收入金額'] < 10000) {
+                continue;
+            }
+            if($data['收支科目'] == '營利事業捐贈收入') {
+                $targetFile = $pathBiz . '/' . $data['統一編號'] . '.csv';
+            } else {
+                $targetFile = $pathIdv . '/' . substr($data['統一編號'], 0, 3) . $data['捐贈者／支出對象'] . '.csv';
+            }
+            
+            if (!file_exists($targetFile)) {
+                $oFh = fopen($targetFile, 'w');
+                fputcsv($oFh, ['選舉', '捐贈對象', '捐贈人', '捐贈日期', '捐贈金額']);
+                fclose($oFh);
+            }
+            $oFh = fopen($targetFile, 'a');
+            fputcsv($oFh, ['105年立法委員選舉', $data['候選人'], $data['捐贈者／支出對象'], $data['交易日期'], $data['收入金額']]);
+            fclose($oFh);
+        }
+    }
+}
+
 /**
  *     [0] => 序號
     [1] => 擬參選人／政黨
