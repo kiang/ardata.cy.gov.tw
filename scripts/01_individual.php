1020f34f347fb92473a11785ecca736d7328e6a2
kiang <kiange@gmail.com>
2025-03-17 09:16:50 +0800

    pretty print for lists

--- scripts/01_individual.php
+++ scripts/01_individual.php
@@ -8,7 +8,7 @@ $pageCount = 1;
 for ($i = 1; $i <= $pageCount; $i++) {
     $listFile = $dataPath . "/page_{$i}.json";
     $c = exec("curl 'https://ardata.cy.gov.tw/api/v1/search/individuals?page={$i}&pageSize=1000&keyword=&' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Referer: https://ardata.cy.gov.tw/data/search/individual' -H 'Save-Data: on' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache'");
-    file_put_contents($listFile, $c);
+    file_put_contents($listFile, json_encode(json_decode($c), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
     $accounts = json_decode(file_get_contents($listFile), true);
     if ($pageCount == 1) {
         $pageCount = $accounts['paging']['pageCount'];
